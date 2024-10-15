fdf59bd48aa168f450c33938978c64af34cd3d4c
LuizFillip <luizfillip@outlook.com>
2024-10-14 22:51:57 -0300

    keogram and find nucleos functions

--- src/core.py
+++ src/core.py
@@ -8,61 +8,68 @@ import matplotlib.pyplot as plt
 import GOES 
 from matplotlib import cm
 import cartopy.crs as ccrs
+from tqdm import tqdm 
+import os 
+from scipy.ndimage import label, find_objects
 
 b.config_labels()
 
 
-def mapping_plot(data, dlon, dlat, ax = None):
+def goes_cmap():
+    
+    path = 'GOES/src/colorbar.cpt'
+    
+    return cm.colors.LinearSegmentedColormap(
+        'cpt', GOES.loadCPT(path)) 
+
+
+def mapping_plot(data, lons, lats, ax = None, ref_lon = None):
 
     lat_lims = dict(min = -40, max = 20, stp = 10)
     lon_lims = dict(min = -90, max = -30, stp = 10) 
-    
-   
-    path = 'GOES/src/IR4AVHRR6.cpt'
-    
-    cmap = cm.colors.LinearSegmentedColormap(
-        'cpt', GOES.loadCPT(path)) 
-    
+
     if ax is None:
     
         fig, ax = plt.subplots(
                  figsize = (12, 12), 
                  dpi = 300, 
-                 subplot_kw = 
-                 {'projection': ccrs.PlateCarree()}
+                 # subplot_kw = 
+                 # {'projection': ccrs.PlateCarree()}
                  )
     
-    gg.map_attrs(
-        ax, 2013, 
-        lat_lims  = lat_lims, 
-        lon_lims = lon_lims,
-        grid = False,
-        degress = None
+    # gg.map_attrs(
+    #     ax, 2013, 
+    #     lat_lims  = lat_lims, 
+    #     lon_lims = lon_lims,
+    #     grid = False,
+    #     degress = None
+    #     )
+    
+    
+    # img = ax.contourf(dlon, dlat, data, 30, cmap = goes_cmap())
+    img = ax.imshow(
+        data, 
+        aspect = 'auto',
+        # extent = [lons[0], lons[-1], lats[0], lats[-1]],
+        cmap = goes_cmap()
         )
+    ticks = np.arange(np.nanmin(data), np.max(data), 20)
+    
+    b.colorbar(
+           img, 
+           ax, 
+           ticks, 
+           label = 'Temperature (°C)', 
+           height = "100%", 
+           width = "10%",
+           orientation = "vertical", 
+           anchor = (.25, 0., 1, 1)
+           )
+    if ref_lon is not None:
+        ax.axvline(ref_lon, color = 'w', lw = 3)
+    return fig, ax
     
     
-    ax.contourf(dlon, dlat, data, 50, cmap = cmap)
-    
-    # plt.colorbar(img)
-    
-    return ax
-    
-# fig, ax = plt.subplots(
-#           figsize = (12, 12), 
-#           dpi = 300, 
-#           subplot_kw = 
-#           {'projection': ccrs.PlateCarree()}
-#           )
-    # mapping_plot(data, dlon, dlat, ax = ax)
- # table = pd.pivot_table(
- #     ds, 
- #     columns = ns[0], 
- #     index = ns[1], 
- #     values = ns[2]
- #     )
- # dlon = table.columns
- # dlat = table.index
-        
         
 def read_gzbin(f_name):
  
@@ -85,12 +92,6 @@ def fname2date(fname):
     return dt.datetime.strptime(dn, '%Y%m%d%H%M')
 
 
-
-
-fname = 'GOES/data/S10236964_201301010000.gz'
-fname = 'GOES/data/S11232404_201312010800.gz'
-fname = 'GOES/data/S10236964_201306020530.gz'
-
 def structured_data(nlons, nlats, grid):
     x, y = np.meshgrid(nlons, nlats)
     
@@ -100,98 +101,207 @@ def structured_data(nlons, nlats, grid):
     
     return np.column_stack((x, y, grid_means))
 
-def filter_region(df, year, sector):
-    '''filter region'''
-    corners = gg.set_coords(year)
 
-    xlim, ylim = corners[sector]
-    
-    return df.loc[
-        (df.lon > xlim[0]) & 
-        (df.lon < xlim[1]) & 
-        (df.lat > ylim[0]) & 
-        (df.lat < ylim[1])
-        ]
 
-def binary_to_dataset(fname):
-    data = read_gzbin(fname)
+class CloudyTemperature(object):
     
-    dlon = np.arange(data.shape[1]) * 0.04 - 100
-    dlat = np.arange(data.shape[0]) * 0.04 - 50
+    def __init__(self, fname):
+        self.fname = fname
+        self.data = read_gzbin(fname)
+        shape = self.data.shape
+        self.lon = np.arange(shape[1]) * 0.04 - 100
+        self.lat = np.arange(shape[0]) * 0.04 - 50
+    
         
-    ns = ['lon', 'lat', 'temp']
+    @property
+    def to_dataset(self):
+        data = structured_data(self.lon, self.lat, self.data)
+        ds = pd.DataFrame(
+            data, 
+            columns = ['lon', 'lat', 'temp']
+            )
+        
+        ds['time'] = fname2date(self.fname)
+        
+        return ds 
     
-    return pd.DataFrame(
-        structured_data(dlon, dlat, data), 
-        columns = ns
+    @property
+    def to_pivot(self):
+        ds = self.to_dataset()
+        return pd.pivot_table(
+            ds, 
+            columns = 'lon', 
+            index = 'lat', 
+            values = 'temp'
+            )
+    
+
+
+   
+
+
+def load_files(ref_day):
+    path = 'E:\\database\\goes\\'
+
+    files = os.listdir(path)
+    
+    return [os.path.join(path, f) for f in files 
+            if fname2date(f) < ref_day]
+
+def CloudTopKeogram(fname, lon = -55):
+    out = []
+    
+    for file in tqdm(files, 'MakeKEO'):
+
+        CT = CloudyTemperature(fname)
+        
+        ds = CT.to_dataset()
+        out.append(ds.loc[ds['lon'] == lon])
+     
+    ds = pd.concat(out)
+    
+    # # fig = plot_keogram(ds1, ax = None)
+    # time = ds1.columns
+    # time = [b.dn2float(t, sum_from = None) for t in time]
+    # lats = ds1.index
+    # data = ds1.values[::-1]
+    return pd.pivot_table(
+        ds, 
+        values = 'temp', 
+        columns = 'time', 
+        index = 'lat'
         )
 
-def get_mean_sum(df, dn):
+
+
+
+
+
+def plot_colobar(ticks, img, ax):
+
     
-    out_mean = {}
-    out_sum = {}
-    for sector in np.arange(-80, -40, 10):
-        
-        ds = filter_region(df, dn.year, sector)
- 
-        data = ds['temp'].values
-     
-        out_sum[sector] = np.nansum(data)
-        out_mean[sector] = np.nanmean(data)
-    
-    df_mean = pd.DataFrame(out_mean, index = [dn])
-    df_sum = pd.DataFrame(out_sum, index = [dn])
 
-    return df_mean, df_sum
-
-
-import os 
-from time import time
-import Webscrape as wb 
-
-dn = dt.datetime(2013, 1, 1)
-str_mon = dn.strftime("%m")
-str_yer = dn.strftime("%Y")
-
-url = f"http://ftp.cptec.inpe.br/goes/goes13/retangular_4km/ch4_bin/{str_yer}/{str_mon}/"
-
-path_to_save = 'E:\\database\\goes\\'
-
-out_mean = []
-out_sum = []
-
-s = time()
-for href in wb.request(url):
-    
-    if href.endswith('gz'):
-        
-        print('downloading', href)
-        
-        wb.download(
-            url, 
-            href, 
-            path_to_save
+    b.colorbar(
+            img, 
+            ax, 
+            ticks, 
+            label = 'Temperature (°C)', 
+            height = "100%", 
+            width = "5%",
+            orientation = "vertical", 
+            anchor = (0.1, 0., 1, 1)
             )
-        
-        fname = os.path.join(
-            path_to_save, href
+    
+    b.colorbar(
+            img, 
+            ax, 
+            ticks, 
+            label = 'Temperature (°C)', 
+            height = '10%' , 
+            width = "80%",
+            orientation = "horizontal", 
+            anchor = (-0.26, 0.7, 1.26, 0.55)
             )
-        dn = fname2date(fname)
+    
+    return 
+
+
+def find_nucleos(
+        data, 
+        lons, 
+        lats,
+        ax = None,
+        area_treshold = 40,
+        step = 0.5, 
+        by_indexes = True
+        ):
         
-        print('processing', dn)
-
-        df_mean, df_sum = get_mean_sum(
-           binary_to_dataset(fname), dn)
+    labeled_array, num_features = label(~np.isnan(data))
         
-        out_mean.append(df_mean)
-        out_sum.append(df_sum)
+    ymax, xmax = data.shape
+    
+    for i, region in enumerate(find_objects(labeled_array)):
+       
+        x_stt, x_end = region[1].start, region[1].stop
+        y_stt, y_end = region[0].start, region[0].stop
         
-ds1 = pd.concat(out_sum)
-ds = pd.concat(out_mean)
+        
+        area = (y_end - y_stt) * (x_end - x_stt)
+        
+        if area > area_treshold:
+            
+            if by_indexes:
+                if (x_end == xmax):
+                    x_end = -1
+                if (y_end == ymax):
+                    y_end = -1
+                
+                x_stt, x_end = lons[x_stt], lons[x_end]
+                y_stt, y_end = lats[y_stt], lats[y_end] 
+            
+            if ax is not None:
+                rect = plt.Rectangle(
+                    (x_stt, y_stt), 
+                    x_end - x_stt, 
+                    y_end - y_stt,
+                    edgecolor = 'red', 
+                    facecolor = 'none', 
+                    linewidth = 2 
+                    )
+            
+                ax.add_patch(rect)
+                # middle_y = (y_end + y_stt) / 2
+                # middle_x = (x_end + x_stt) / 2
+                # ax.text(
+                #     middle_x, 
+                #     middle_y + 1, i, 
+                #     transform = ax.transData
+                #     )
+    return
+        
+ref_day = dt.datetime(2013, 1, 2)
+files = load_files(ref_day)
 
-ds.to_csv('mean_convect')
-ds1.to_csv('sum_convect')
+fname = files[0]
+ds = CloudyTemperature(fname)
+data = ds.data[::-1]
+lons = ds.lon 
+lats = ds.lat
 
-e = time()
 
-print((e - s) / 3600)
+fig, ax = plt.subplots(
+    dpi = 300, 
+    figsize = (16, 12)
+    )
+
+
+img = ax.imshow(
+    data,
+    aspect = 'auto', 
+    extent = [lons[0], lons[-1], lats[0], lats[-1]],
+    cmap = goes_cmap()
+    
+    )
+
+ticks = np.arange(np.nanmin(data), np.max(data), 20)
+b.colorbar(
+       img, 
+       ax, 
+       ticks, 
+       label = 'Temperature (°C)', 
+       height = "100%", 
+       width = "5%",
+       orientation = "vertical", 
+       anchor = (.1, 0., 1, 1)
+       )
+data = np.where(data > -40, np.nan, data)
+
+find_nucleos(
+        data, 
+        lons, 
+        lats[::-1],
+        ax,
+        area_treshold = 60,
+        step = 0.5
+        )
+
