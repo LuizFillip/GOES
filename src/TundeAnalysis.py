701788324f07cef72110d5487ea67f1c6a5aab85
LuizFillip <luizfillip@outlook.com>
2024-11-14 13:48:35 -0300

    plot Ep sazonality

--- src/TundeAnalysis.py
+++ src/TundeAnalysis.py
@@ -3,6 +3,8 @@ import cartopy.crs as ccrs
 import GEO as gg
 import matplotlib.pyplot as plt
 import base as b 
+from scipy.ndimage import gaussian_filter
+import numpy as np 
 
 def load_tunde(infile):
     df = pd.read_csv(infile, delim_whitespace=True)
@@ -21,58 +23,25 @@ def load_tunde(infile):
     return df
 
 
-def plot_map():
-    fig, ax = plt.subplots(
-          dpi = 300, 
+
     
-          figsize = (10,10),
-          subplot_kw = 
-          {'projection': ccrs.PlateCarree()}
-          )
-        
+
+
+def load_nucleos():
+    infile = 'GOES/data/2013'
     
-    lat_lims = dict(min = -40, max = 20, stp = 10)
-    lon_lims = dict(min = -90, max = -30, stp = 10) 
+    df = b.load(infile)
     
-    gg.map_attrs(
-       ax, 2013, 
-       lat_lims = lat_lims, 
-       lon_lims = lon_lims,
-       grid = False,
-       degress = None
-        )
+    df['lon'] = (df['x1'] + df['x0']) / 2
+    df['lat'] = (df['y1'] + df['y0']) / 2
+    
+    #
+    cond = [   (df['lat'] > -20) & 
+        (df['lat'] < 10) ]
+    df = df.resample(sample).size()
     
     
-    ax.scatter(
-        df['Lon'],
-        df['Lat'], 
-        c = df['mean_90_110']
-        )
-
-infile = 'GOES/data/Select_ep_data_lat_lon_2013.txt'
-
-ds = load_tunde(infile)
-
-ds = ds.loc[(ds['Lat'] > -20) & 
-            (ds['Lat'] < 10)]
-
-sample = '5D'
-ds = ds['mean_90_110'].resample(sample).mean()
-
-
-infile = 'GOES/data/2013'
-
-df = b.load(infile)
-
-df['lon'] = (df['x1'] + df['x0']) / 2
-df['lat'] = (df['y1'] + df['y0']) / 2
-
-df = df.loc[
-    (df['lat'] > -20) & 
-    (df['lat'] < 10) ].resample(sample).size()
-
-
-df['percent'] = (df / df.values.max()) *100
+    df = (df / df.values.max()) *100
 
 
 def plot_seasonal(df, ds):
@@ -82,12 +51,11 @@ def plot_seasonal(df, ds):
           figsize = (16, 8)
           )
         
-    ax.scatter(df.index, 
-               df['percent'], c = 'b' )
-    df = df.rolling(10).mean()
+    sdf = gaussian_filter(df, sigma=1)
+
     ax.plot(
-        df.index, 
-        df, 
+        df.index,
+        sdf, 
             lw = 3, color = 'blue')
     ax.set(ylabel = 'Convective activity (\%)')
     
@@ -99,8 +67,11 @@ def plot_seasonal(df, ds):
             )
     
     ax1 = ax.twinx()
-    ax.scatter(ds.index, ds.values, c = 'red')
-    ax1.plot(ds.index, b.smooth2(ds, 10), lw = 3, color = 'red')
+    # ax1.scatter(ds.index, ds.values, c = 'red')
+    
+    # ds = ds.rolling(10).mean()
+    sds = gaussian_filter(ds, sigma=1)
+    ax1.plot(ds.index, sds,  lw = 3, color = 'red')
     
     ax1.set(ylabel = 'GW potential energy (J/Kg)')
     b.change_axes_color(
@@ -114,6 +85,177 @@ def plot_seasonal(df, ds):
     b.format_month_axes(ax)
 
 
-plot_seasonal(df, ds)
+# plot_seasonal(df, ds)
 
-ds
+
+def size_by_grid(
+        df, step = 1, 
+        rounding = 0):
+ 
+    lon_bins = np.arange(
+        df['lon'].min(), 
+        df['lon'].max() + step, step
+        )
+    lat_bins = np.arange(
+        df['lat'].min(), 
+        df['lat'].max() + step, step
+        )
+    
+    df['lon_bin'] = pd.cut(
+        df['lon'], 
+        bins=lon_bins, 
+        labels=lon_bins[:-1]
+        )
+    df['lat_bin'] = pd.cut(
+        df['lat'], 
+        bins=lat_bins, 
+        labels=lat_bins[:-1]
+        )
+    
+    df['lon_bin'] = df['lon_bin'].astype(
+        float).round(rounding)
+    df['lat_bin'] = df['lat_bin'].astype(
+        float).round(rounding)
+
+      
+    event_count = df.groupby(
+        ['lon_bin', 'lat_bin']
+        ).mean().reset_index()
+    
+    return event_count
+
+
+
+
+def plot_map(ax, ds):
+    # fig, ax = plt.subplots(
+    #       dpi = 300, 
+    #       figsize = (10,10),
+    #       subplot_kw = 
+    #       {'projection': ccrs.PlateCarree()}
+    #       )
+        
+    
+    lat_lims = dict(min = -40, max = 20, stp = 10)
+    lon_lims = dict(min = -90, max = -30, stp = 10) 
+    
+    gg.map_attrs(
+       ax, 2013, 
+       lat_lims = lat_lims, 
+       lon_lims = lon_lims,
+       grid = False,
+       degress = None
+        )
+
+
+    img = ax.contourf(
+        ds.columns, 
+        ds.index, 
+        ds.values, 
+        levels = np.arange(3, 18, 1),
+        cmap = 'jet'
+        )
+    
+    # ticks= np.arange(3, 18, 2)
+    
+    # b.colorbar(
+    #     img,  
+    #     ax,   
+    #     ticks, 
+    #     label = 'Ep (J/Kg)', 
+    #     height = "100%", 
+    #     width = "10%",
+    #     orientation = "vertical", 
+    #     anchor = (.25, 0., 1, 1)
+    #     )
+    
+    
+
+def set_data(ds, step = 1):
+    
+    df = size_by_grid(
+            ds, step = step, 
+            rounding = 0
+            )
+
+    ds = pd.pivot_table(
+        df, 
+        columns = 'lon_bin', 
+        index = 'lat_bin', 
+        values = 'mean_90_110'
+        )
+    
+    # ds = ds.replace(np.nan, 0)
+    
+    return ds 
+
+months = [
+    [12, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8],
+    [9, 10, 11]
+    ]
+
+names = [
+    'dez - fev', 
+    'mar - mai', 
+    'jun - agu',
+    'set - nov'
+    ]
+fig, ax = plt.subplots(
+      dpi = 300, 
+      ncols = 2, 
+      nrows = 2, 
+      figsize = (16, 16),
+      subplot_kw = 
+      {'projection': ccrs.PlateCarree()}
+      )
+
+plt.subplots_adjust(wspace = 0., hspace = 0.15)
+
+year = 2013
+infile = 'GOES/data/Select_ep_data_lat_lon_2013.txt'
+
+ds = load_tunde(infile)
+
+
+ds = ds.rename(columns = {'Lat': 'lat', 'Lon': 'lon'})
+
+b.config_labels()
+
+step = 2
+
+
+for i, ax in enumerate(ax.flat):
+    
+    season = months[i]
+    
+    df = ds.loc[ds.index.month.isin(season)]
+          
+    plot_map(ax, set_data(df, step = step)) 
+    
+    if i != 2:
+        
+        ax.set(
+            xticklabels = [],
+            xlabel = '',
+            ylabel = '',
+            yticklabels = []
+            )
+
+    
+    ax.set(title = names[i].upper())
+    
+    
+b.fig_colorbar(
+        fig,
+        label = 'Ep (J/Kg)',
+        fontsize = 35,
+        vmin = 3, 
+        vmax = 18, 
+        step = 2,
+        orientation = 'horizontal',
+        sets = [0.13, 1., 0.75, 0.02] 
+        )
+
+fig.suptitle(year)
