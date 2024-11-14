42dc6ab716a44fb5f52e1969a1e6eafb8c27ad03
LuizFillip <luizfillip@outlook.com>
2024-11-14 13:49:23 -0300

    get percent of nucleos

--- src/ConvectiveAvg.py
+++ src/ConvectiveAvg.py
@@ -2,45 +2,48 @@ import cartopy.crs as ccrs
 import GEO as gg
 import matplotlib.pyplot as plt
 import base as b 
-import GOES as gs
 import numpy as np 
-import math 
 import pandas as pd 
+
+b.config_labels(fontsize = 35 )
 
 
 def size_by_grid(
-        df, lon_step=2.5, lat_step=2.5, 
+        df, 
+        step = 2.5, 
         rounding = 0):
- 
+  
     lon_bins = np.arange(
         df['lon'].min(), 
-        df['lon'].max() + lon_step, lon_step
+        df['lon'].max() + step, step
         )
     lat_bins = np.arange(
         df['lat'].min(), 
-        df['lat'].max() + lat_step, lat_step
+        df['lat'].max() + step, step
         )
     
-    df['lon_bin'] = pd.cut(
-        df['lon'], 
+    df['lon_bin'] = pd.cut( df['lon'], 
         bins=lon_bins, 
         labels=lon_bins[:-1]
         )
-    df['lat_bin'] = pd.cut(
-        df['lat'], 
+    df['lat_bin'] = pd.cut(df['lat'], 
         bins=lat_bins, 
         labels=lat_bins[:-1]
         )
     
-    df['lon_bin'] = df['lon_bin'].astype(float).round(rounding)
-    df['lat_bin'] = df['lat_bin'].astype(float).round(rounding)
+    df['lon_bin'] = df['lon_bin'].astype(
+        float).round(rounding)
+    df['lat_bin'] = df['lat_bin'].astype(
+        float).round(rounding)
 
       
-    event_count = df.groupby(['lon_bin', 'lat_bin']).size().reset_index(name='event_count')
+    event_count = df.groupby(
+        ['lon_bin', 'lat_bin']).size(
+            ).reset_index(name='event_count')
     
     return event_count
 
-def plot(ax, df):
+def plot_map_contour(ax, df):
 
     
     lat_lims = dict(min = -40, max = 20, stp = 10)
@@ -53,70 +56,43 @@ def plot(ax, df):
        grid = False,
        degress = None
         )
-    gg.plot_rectangles_regions(ax, 2013)
-    img = ax.contourf(
+    gg.plot_rectangles_regions(ax, 2013, color = 'white')
+    
+    ax.contourf(
         df.columns, 
         df.index, 
         df.values, 
-        10, 
-        cmap = 'jet')
-    
-    # ticks= np.arange(0, 110, 20)
-    
-    # b.colorbar(img,  ax,     ticks, 
-    #       label = 'TECU ($10^{16}/m^2$)', 
-    #       height = "100%", 
-    #       width = "10%",
-    #       orientation = "vertical", 
-    #       anchor = (.25, 0., 1, 1)
-    #       )
-
-    # ax.scatter(
-    #     df['lon_bin'], 
-    #     df['lat_bin'], 
-    #     c = df['event_count'], 
-    #     cmap = 'jet'
-    #     )
+        levels = np.arange(0, 100, 1), 
+        cmap = 'jet'
+        )
     
     return fig
 
-def to_pivot(ds):
-    ds = pd.pivot_table(
-        ds, columns = 'lon_bin', index = 'lat_bin', 
-                    values = 'event_count')
-    
-    # ds = ds.replace(np.nan, 0)
-    
-    ds = (ds / np.nanmax(ds.values)) * 100 
-    
-    return ds
-    
-
-fname = 'E:/database/goes/2019/04/S10635346_201904010000.nc'
 
 
-# figs = gs.test_plot(fname, temp = -60)
-
-dn = gs.fname2date(fname)
-infile = 'GOES/data/2019/'
-infile = 'GOES/data/2018'
-
-def set_data(df):
+def set_data(df, step = 2):
     df['lon'] = (df['x1'] + df['x0']) / 2
     df['lat'] = (df['y1'] + df['y0']) / 2
     
+    ds = size_by_grid(df, step = step).dropna()
     
-    df = df.loc[df['area'] > 10, ['area', 'lon', 'lat']]
+    ds = pd.pivot_table(
+        ds, 
+        columns = 'lon_bin', 
+        index = 'lat_bin', 
+        values = 'event_count'
+        )
     
+    ds = ds.replace(np.nan, 0)
     
-    ds = size_by_grid(
-        df, lon_step = 1, lat_step = 1).dropna()
+    ds = (ds / np.nanmax(ds.values)) * 100 
     
-    return to_pivot(ds)
-# fig = plot(ds) 
+    return ds 
 
-# ds
 
+
+year = 2013
+infile = f'GOES/data/{year}'
 
 
 df = b.load(infile)
@@ -134,6 +110,7 @@ names = [
     'jun - agu',
     'set - nov'
     ]
+
 fig, ax = plt.subplots(
       dpi = 300, 
       ncols = 2, 
@@ -143,15 +120,15 @@ fig, ax = plt.subplots(
       {'projection': ccrs.PlateCarree()}
       )
     
+plt.subplots_adjust(wspace = 0., hspace = 0.15)
 
 
 for i, ax in enumerate(ax.flat):
     
     season = months[i]
-    ds = set_data(
-        df.loc[df.index.month.isin(season)])
+    ds = set_data(df.loc[df.index.month.isin(season)])
         
-    plot(ax, ds) 
+    plot_map_contour(ax, ds) 
     
     if i != 2:
         
@@ -159,7 +136,8 @@ for i, ax in enumerate(ax.flat):
             xticklabels = [],
             xlabel = '',
             ylabel = '',
-            yticklabels = [])
+            yticklabels = []
+            )
 
     
     ax.set(title = names[i].upper())
@@ -167,11 +145,13 @@ for i, ax in enumerate(ax.flat):
     
 b.fig_colorbar(
         fig,
-        label = 'Convective Nucleos (\%)',
+        label = 'Occurrence rate of convective Nucleos (\%)',
         fontsize = 35,
         vmin = 0, 
         vmax = 100, 
-        step = 20,
+        step = 10,
         orientation = 'horizontal',
-        sets = [0, 1., 0.95, 0.02] 
+        sets = [0.14, 1., 0.75, 0.02] 
         )
+
+fig.suptitle(year, y = 1.1)
