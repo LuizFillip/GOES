3d1369538b99342496ab7ea2bd273ad2c43b9d3f
LuizFillip <luizfillip@outlook.com>
2025-01-30 16:46:36 -0300

    updates

--- src/TundeAnalysis.py
+++ src/TundeAnalysis.py
@@ -1,20 +1,48 @@
 import pandas as pd 
-import cartopy.crs as ccrs
 import GEO as gg
 import matplotlib.pyplot as plt
 import base as b 
-from scipy.ndimage import gaussian_filter
 import numpy as np 
 import core as c
+import GOES as gs
+import cartopy.crs as ccrs
 
+def plot_seasonal_Ep():
 
-
+    fig, ax = plt.subplots( 
+        sharex = True,
+        sharey = True,
+        figsize = (14, 8), 
+        dpi = 300
+        )
+        
+    years = [2013, 2019]
+    maks = ['s', 'o']
+    labl = ['Maximum', 'Minimum']
     
-
-
-
-
-
+    for i, year in enumerate(years):
+        
+        df = c.potential_energy(year)
+        
+        ds = df['mean_90_110'].resample('1M').mean()
+        
+        ds.index = ds.index.month
+        
+        ax.plot(ds, lw = 2, 
+                label = f'Solar {labl[i]} - {year}',
+                markersize = 20, 
+                marker = maks[i], 
+             
+                )
+    
+    ax.legend(loc = 'upper right')
+    ax.set(
+           ylabel = 'Ep (J/Kg)', 
+           xticks = np.arange(1, 13, 1),
+           xlabel = 'Months')
+    
+    plt.show()
+    return fig
 
 def size_by_grid(
         df, 
@@ -60,7 +88,7 @@ def size_by_grid(
 def plot_map(ax, ds, year = 2013):
 
     lat_lims = dict(min = -40, max = 20, stp = 10)
-    lon_lims = dict(min = -90, max = -30, stp = 10) 
+    lon_lims = dict(min = -80, max = -40, stp = 10) 
     
     gg.map_attrs(
        ax, 
@@ -77,17 +105,18 @@ def plot_map(ax, ds, year = 2013):
         ds.columns, 
         ds.index, 
         ds.values, 
-        levels = np.arange(3, 20, 1),
+        # levels = np.arange(3, 20, 5),
         cmap = 'jet'
         )
     
     return img 
     
 
-def set_data(ds, step = 1):
+def set_data(ds, step = 3, values = 'mean_60_90'):
     
     df = size_by_grid(
-            ds, step = step, 
+            ds, 
+            step = step, 
             rounding = 0
             )
 
@@ -95,88 +124,51 @@ def set_data(ds, step = 1):
         df, 
         columns = 'lon_bin', 
         index = 'lat_bin', 
-        values = 'mean_90_110'
+        values = values
         )
-    # print(np.nanmax(ds.values))
+
     return ds 
 
-def plot_seasonal_Ep():
 
-    fig, ax = plt.subplots( 
-        sharex = True,
-        sharey = True,
-        figsize = (14, 8), 
-        dpi = 300
-        )
-        
-    years = [2013, 2019]
-    maks = ['s', 'o']
-    labl = ['Maximum', 'Minimum']
-    
-    for i, year in enumerate(years):
-        
-        df = c.potential_energy(year)
-        
-        ds = df['mean_90_110'].resample('1M').mean()
-        
-        ds.index = ds.index.month
-        
-        ax.plot(ds, lw = 2, 
-                label = f'Solar {labl[i]} - {year}',
-                markersize = 20, 
-                marker = maks[i], 
-             
-                )
-    
-    ax.legend(loc = 'upper right')
-    ax.set(
-           ylabel = 'Ep (J/Kg)', 
-           xticks = np.arange(1, 13, 1),
-           xlabel = 'Months')
-    
-    plt.show()
-    return fig
 
 def plot_seasonal_occurrence(
         ax, 
-        row = 1, 
+        ds,
         year = 2013,
         step = 2
         ):
     
-    ds = c.potential_energy(year)
-    
-    ds = ds.rename(columns = {'Lat': 'lat', 
-                              'Lon': 'lon'})
-
-    
+   
     seasons = {
-        'Nov - Jan': [11, 12, 1], 
-        'Feb - Apr': [2, 3, 4], 
-        'Jun - Sep': [5, 6, 7],
-        'Agu - Oct': [8, 9, 10]
-        }
 
+        'Jan - Feb': [1, 2], 
+        'Mar - Apr': [3, 4], 
+        'May - Jun': [5, 6], 
+        'Jul - Aug': [7, 8], 
+        'Sep - Oct': [9, 10], 
+        'Nov - Dec': [11, 12]
+    }
+    
+    axes = ax.flat
 
     for i, (key, value) in enumerate(seasons.items()):
        
         df = ds.loc[ds.index.month.isin(value)]
         
-              
-        plot_map(ax[row, i], set_data(df, step = step)) 
+        plot_map(axes[i], set_data(df, step = step)) 
                     
         l = b.chars()[i]
         
-        ax[row, i].set_title(
+        axes[i].set_title(
             f'({l}) {key} ({year})', 
             fontsize = 20
             )
             
         
-        if i == 0 and row == 1:
+        if i == 3:
             pass
         else:
-            ax[row, i].set(
+            axes[i].set(
                 xticklabels = [],
                 xlabel = '',
                 ylabel = '',
@@ -185,36 +177,45 @@ def plot_seasonal_occurrence(
             
     return ax 
 
+       
         
-        
-        
-        
-# b.config_labels(fontsize = 25)
+b.config_labels(fontsize = 25)
     
+fig, axs = plt.subplots(
+      dpi = 300, 
+      ncols = 3, 
+      nrows = 2, 
+      figsize = (13, 14),
+      subplot_kw = 
+      {'projection': ccrs.PlateCarree()}
+      )
 
-# fig, axs = plt.subplots(
-#       dpi = 300, 
-#       ncols = 4, 
-#       nrows = 2, 
-#       figsize = (15, 8),
-#       subplot_kw = 
-#       {'projection': ccrs.PlateCarree()}
-#       )
+plt.subplots_adjust(wspace = 0.1, hspace = 0.05)
 
-# plt.subplots_adjust(wspace = 0.1, hspace = 0.1)
-    
-# plot_seasonal_occurrence(axs, row = 0, year = 2013, step = 3)
-# plot_seasonal_occurrence(axs, row = 1, year = 2019, step = 3)
+ds = b.load('GOES/data/ep_all')
 
-# b.fig_colorbar(
-#         fig,
-#         label = 'Ep (J/Kg)',
-#         fontsize = 35,
-#         vmin = 3, 
-#         vmax = 18, 
-#         step = 2,
-#         orientation = "vertical", 
-#         anchor = (.94, 0.13, 0.03, 0.73)
-#         )
+ds = gs.limits(
+    ds,  
+    x0 = -90, x1 = -30, 
+    y0 = -40, y1 = 20
+    )
 
-# # fig.suptitle(year, y = 1.1)
+plot_seasonal_occurrence(axs, ds, year = 2013, step = 4)
+
+b.fig_colorbar(
+        fig,
+        label = 'Ep (J/Kg)',
+        fontsize = 35,
+        vmin = 3, 
+        vmax = 18, 
+        step = 2,
+        orientation = "vertical", 
+        anchor = (.94, 0.13, 0.03, 0.73)
+        )
+
+# fig.suptitle(year, y = 1.1)
+
+year = 2015
+df = gs.potential_energy(year = year)
+
+# df
