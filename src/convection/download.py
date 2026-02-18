d6bd2bbfaaf490ab782b057d871ee363f56815d0
LuizFillip <luizfillip@outlook.com>
2026-02-18 19:16:26 -0300

    replacing old functions

--- src/convection/download.py
+++ src/convection/download.py
@@ -2,6 +2,7 @@ import base as b
 import os 
 import Webscrape as wb 
 from tqdm import tqdm 
+import datetime as dt 
 
 def goesURL(str_yr, str_mn):
     
@@ -18,40 +19,49 @@ def goesURL(str_yr, str_mn):
         
     return f'{base}{str_yr}/{str_mn}/'
 
+def fn2dn(fn):
+    fmt = '%Y%m%d%H%M.nc'
+    date_string = fn.split('_')[1]
+    return dt.datetime.strptime(date_string, fmt)
 def dowloadGOES(dn,  B = 'E'):
     
     str_mn = dn.strftime("%m")
     str_yr = dn.strftime("%Y")
     
     root = f'{B}:\\database\\goes\\'
+    b.make_dir(root)
     path_yr = os.path.join(root, str_yr)
     b.make_dir(path_yr)
     path_mn = os.path.join(path_yr, str_mn)
     b.make_dir(path_mn)
     url = goesURL(str_yr, str_mn)
     
-    info = 'Download'
-    out = []
-    
+    desc = f'Download - {dn.date()}'
+  
     files = os.listdir(path_mn)
     
-    for href in tqdm(wb.request(url), info):
+    for href in tqdm(wb.request(url), desc):
    
         if href.endswith('gz') or href.endswith('nc'):
             
             if href not in files:
-                out.append(href)
+                # out.append(href)
+                
+                if fn2dn(href).minute == 0:
+                    # print(fn2dn(href))
             
-                wb.download(
-                    url, 
-                    href, 
-                    path_mn
-                    )
+                    wb.download(
+                        url, 
+                        href, 
+                        path_mn
+                        )
             else:
                 pass
             
    
-    return path_mn
+    return None 
 
+for month in list(range(3, 13)):
 
-    
+    dn = dt.datetime(2023, month, 1)
+    dowloadGOES(dn, B = 'D')
