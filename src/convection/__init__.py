7479d67eb9b5624126451ec945a4cf5f09739016
LuizFillip <luizfillip@outlook.com>
2026-02-20 22:38:11 -0300

    updates

--- src/convection/__init__.py
+++ src/convection/__init__.py
@@ -1,6 +1,6 @@
 from .cpt_convert import *
 from .core import *
 from .nucleo_detection import find_nucleos 
-# from .download import dowloadGOES
+from .download import image_url, imshow_url 
 from .dirs import *
 from .areas import *
--- src/convection/dirs.py
+++ src/convection/dirs.py
@@ -1,15 +1,15 @@
 import os 
 import datetime as dt
 
-def load_files(ref_day):
-    path = 'E:\\database\\goes\\'
+# def load_files(ref_day):
+#     path = 'E:\\database\\goes\\'
 
-    files = os.listdir(path)
+#     files = os.listdir(path)
 
-    if ref_day is not None:
-        files  = [f for f in files if fname2date(f) < ref_day]
+#     if ref_day is not None:
+#         files  = [f for f in files if fname2date(f) < ref_day]
    
-    return [os.path.join(path, f) for f in files]
+#     return [os.path.join(path, f) for f in files]
 
 def walk_goes(dn, B = 'E'):
     
@@ -24,3 +24,11 @@ def fn2dn(fn):
     fmt = '%Y%m%d%H%M'
     date_string = fn.split('_')[1][:-3]
     return dt.datetime.strptime(date_string, fmt)
+
+
+dn = dt.datetime(2012, 1, 2, 3)
+
+def get_path_by_dn(dn):
+    files = walk_goes(dn, B = 'D')
+    
+    return [f for f in files if fn2dn(f) == dn][0]
