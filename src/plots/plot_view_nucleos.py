1db3f7c28283459db5750dd036f53be8719c02b3
LuizFillip <luizfillip@outlook.com>
2026-03-02 14:14:03 -0300

    single plot for nucleos

--- src/plots/plot_view_nucleos.py
+++ src/plots/plot_view_nucleos.py
@@ -7,12 +7,15 @@ import numpy as np
 
 
 def plot_view_nucleos(
-        lon, lat, 
-        temp, dn,
-        threshold=-40,
-        dot_size = None
+        fn,
+        threshold=-40 
         ):
- 
+    
+    lon, lat, temp = gs.read_gzbin(fn)
+    
+    threshold = -40
+    
+    dn = gs.fn2dn(fn)
     nl = gs.find_nucleos(       
         lon,
         lat,
@@ -29,41 +32,20 @@ def plot_view_nucleos(
         lon_max = -30, 
         lat_min = -55, 
         lon_min = -100
-        ) 
-    if dot_size is not None:
+        )  
         
-        for _, row in nl.iterrows():
-            x0, x1 = row["lon_min"], row["lon_max"]
-            y0, y1 = row["lat_min"], row["lat_max"]
-            
-            gs.add_ellipse_from_bbox(
-                ax,
-                x0, x1,
-                y0, y1
-                )
-          
-           
-    return fig, ax 
+    for _, row in nl.iterrows():
+        x0, x1 = row["lon_min"], row["lon_max"]
+        y0, y1 = row["lat_min"], row["lat_max"]
+        
+        gs.add_ellipse_from_bbox(
+            ax,
+            x0, x1,
+            y0, y1
+            )
+    
+        
+    return fig 
 
 
-# --------- example usage ---------
-def example_usage():
-    dn = dt.datetime(2013, 2, 1)
  
-    fn = gs.get_path_by_dn(dn)
-    
-    lon, lat, temp = gs.read_gzbin(fn)
-    
-    threshold = -40
-    
-    fig, ax = plot_view_nucleos(
-        lon, lat, temp, dn,
-        threshold=threshold, 
-        dot_size = 5
-        )
-    
-    
-    plt.show()
- 
-
-example_usage()
