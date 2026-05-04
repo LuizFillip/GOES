d04b16f8fd779ec10ad01d4127e27160769fc146
LuizFillip <luizfillip@outlook.com>
2026-05-04 13:28:51 -0300

    plot grid raw and gaussian interp

--- src/plots/occurrence_rate_grid.py
+++ src/plots/occurrence_rate_grid.py
@@ -2,6 +2,10 @@ import pandas as pd
 from scipy.ndimage import gaussian_filter
 import base as b 
 import numpy as np 
+import matplotlib.pyplot as plt
+import GEO as gg 
+import cartopy.crs as ccrs
+import datetime as dt 
 
 b.sci_format(fontsize = 20)
  
@@ -78,12 +82,13 @@ def plot_occurrence_rate_grid(
         nl,
         lon_bins,
         lat_bins, 
+        n_total,
         cmap = 'jet', 
         vmax = 30, 
         sigma = None 
         ):
     
-    n_total = len(nl.index.unique())
+   
 
     grid = occurrence_rate_grid(
         nl,
@@ -105,8 +110,89 @@ def plot_occurrence_rate_grid(
         vmin = 0, 
         # vmax = vmax
     ) 
+    
+    print(values.max())
     return img 
     
  
 
+df = b.load('GOES/data/nucleos_40/2013')
+
+def set_bins(nl, step = 4):
+    
+    lat_min = np.round(nl['lat_min'].min())
+    lon_min = np.round(nl['lon_min'].min())
+    lon_max = np.round(nl['lon_max'].max())
+    lat_max = np.round(nl['lat_max'].max())
+    
+    
+    lon_bins = np.arange(lon_min, lon_max + step, step)
+    lat_bins = np.arange(lat_min, lat_max + step, step)
+    return lon_bins, lat_bins 
  
+
+def plot_grid_and_interp_maps():
+    fig, ax = plt.subplots(
+         dpi=300,
+         figsize=(10, 10),
+         ncols = 2,
+         subplot_kw={"projection": ccrs.PlateCarree()},
+     )
+    
+    dn = dt.date(2013, 12, 24)
+    
+    lat_lims = dict(min=-60, max=10, stp=10)
+    lon_lims = dict(min=-90, max=-30, stp=10)
+    
+    
+    nl = df.loc[df.index.date == dn]
+    # print(
+    total_month = df.loc[df.index.month == dn.month]
+    n_total = len(total_month.index.unique())
+    # n_total = len(nl.index.unique())
+    
+    lon_bins, lat_bins  = set_bins(nl)
+    
+    sigma = [None, 2]
+    titles = ['Grid 4 x 4', 'Gaussian interpolation']
+    for i, a in enumerate(ax.flat):
+        gg.map_attrs(
+            a,
+            year=None,
+            lat_lims=lat_lims,
+            lon_lims=lon_lims,
+            grid=False,
+            degress=None,
+        )
+        
+        img = plot_occurrence_rate_grid(
+                ax[i],
+                nl,
+                lon_bins,
+                lat_bins, 
+                n_total, 
+                cmap = 'turbo', 
+                vmax = 30, 
+                sigma = sigma[i]
+                )
+        
+        ax[i].set(title = titles[i])
+        # plt.colorbar(img)
+        
+    
+    ax[1].set(yticklabels = [], ylabel = '')
+    
+    b.colorbar(
+            img, 
+            ax[1], 
+            # ticks, 
+            label = 'Occurrence rate (\%)', 
+            height = "100%", 
+            width = "5%",
+            orientation = "vertical", 
+            anchor = (.2, 0., 1, 1)
+            )
+    
+    fig.suptitle(dn.strftime('%Y-%m-%d'), y = 0.8)
+ 
+
